4202a924e220651515841fd68fcfcebf2a8d130f
unknown <gibsonsangk@gmail.com>
2025-12-04 00:23:23 +0300

    fn

--- api/src/server/server.js
+++ api/src/server/server.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = db.connections[0].readyState === 1;
+
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection error:", error.message);
